5a5b5aeec3fea14ff82e940104dbcd76aab89bb6
Ryan Badour <rybadour@gmail.com>
2022-08-26 22:25:08 -0400

    Added some basic link filtering

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import Snoowrap, { Submission } from "snoowrap";
+import fs from 'fs';
+import Snoowrap from "snoowrap";
 import env from "./env";
+import { Game } from './types';
 
 const URL_REGEX = /\[[^\]]+\]\((https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))\)/g;
+
+const LINKS_TO_IGNORE = [
+  'discord.gg/',
+  'discord.com/',
+  'gamewiki.',
+  'reddit.com/',
+  'preview.redd.it/',
+  'prnt.sc',
+];
+
+const GAME_LINKS = [
+  'play.google.com',
+  'store.steampowered.com',
+  'raw.githack.com',
+  'apps.apple.com',
+  'kongregate.com/games/',
+  'itch.io',
+  'github.com',
+];
 
 (async function () {
   const r = new Snoowrap({
@@ -9,28 +30,50 @@ const URL_REGEX = /\[[^\]]+\]\((https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}
     ...env,
   });
 
-  const newGames = {};
-  function getLinks(body: string) {
-    return [...body.matchAll(URL_REGEX)]
-      .map(match => match[1]);
-  }
+  const newGames: Game[] = [];
+  let uncategorizedLinks: string[] = [];
 
-  console.log(r.tokenExpiration);
-  console.log('Rate Remaining: ' + r.ratelimitRemaining);
-
-  /* */
-  r.getSubreddit('incremental_games')
-    .getNew({limit: 20})
+  const parsePosts = r.getSubreddit('incremental_games')
+    .getNew({limit: 50})
     .then((posts) => {
-      console.log('Found ' + posts.length + ' posts');
+      console.log('Analyzing ' + posts.length + ' posts...');
       posts.forEach(post => {
-        const links = getLinks(post.selftext);
+        const links = getLinks(post.selftext)
+          .filter(link => !linkMatches(link, LINKS_TO_IGNORE))
         if (links.length == 0) return;
 
-        console.log(post.title);
-        
-        console.log(links);
+        uncategorizedLinks = uncategorizedLinks.concat(
+          links.filter(link => !linkMatches(link, GAME_LINKS))
+        );
+
+        const gameLinks = links.filter(link => linkMatches(link, GAME_LINKS))
+        if (gameLinks.length > 0) {
+          newGames.push({
+            name: post.title,
+            links: gameLinks,
+          });
+        }
       });
+      
+      return true;
     });
-  /* */
+
+  await Promise.all([parsePosts]);
+
+  console.log('Saving report');
+  fs.writeFileSync('games.json', JSON.stringify(newGames, null, 2));
+
+  if (uncategorizedLinks.length > 0) {
+    console.log('Found ' + uncategorizedLinks.length + ' uncategorized links. Look in uncategorizedLinks.json.');
+  }
+  fs.writeFileSync('uncategorizedLinks.json', JSON.stringify(uncategorizedLinks, null, 2));
 })();
+
+function getLinks(body: string) {
+  return [...body.matchAll(URL_REGEX)]
+    .map(match => match[1]);
+}
+
+function linkMatches(link: string, matches: string[]) {
+  return matches.some(match => link.includes(match));
+}

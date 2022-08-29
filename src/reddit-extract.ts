26e188840cf152ca38587aa93e83da9df11631a4
Ryan Badour <rybadour@gmail.com>
2022-08-29 17:26:41 -0400

    Updated reddit extraction
     - Pulls the name from links.
     - Adds more information to the newGames.json file

---
+++ src/reddit-extract.ts
@@ -0,0 +1,119 @@
+import fs from 'fs';
+
+import Snoowrap from "snoowrap";
+import {partition, uniq, uniqBy} from "lodash";
+
+import env from "./env";
+import { Link, NewGame, Platform } from './types';
+
+const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+const LINK_REGEX = `\\[([^\\]]+)\\]\\((${URL_REGEX.source})\\)`;
+
+
+const LINKS_TO_IGNORE = [
+  'discord.gg/',
+  'discord.com/',
+  'discordapp.net/',
+  'gamewiki.',
+  'reddit.com/',
+  'preview.redd.it/',
+  'prnt.sc',
+];
+
+const GAME_LINKS = [{
+  platform: Platform.Steam,
+  linkMatchers: ['store.steampowered.com'],
+}, {
+  platform: Platform.Android,
+  linkMatchers: ['play.google.com'],
+}, {
+  platform: Platform.Web,
+  linkMatchers: [
+    'raw.githack.com',
+    'kongregate.com/games/',
+    'itch.io',
+    'github.com',
+  ],
+}, {
+  platform: Platform.IOS,
+  linkMatchers: ['apps.apple.com']
+}];
+
+(async function () {
+  const r = new Snoowrap({
+    userAgent: 'nodejs:incremental-game-aggregator:v0.1.0 (by /u/salbris)',
+    ...env,
+  });
+
+  const newGames: NewGame[] = [];
+  let allUncategorizedLinks: Link[] = [];
+
+  const parsePosts = r.getSubreddit('incremental_games')
+    .getNew({limit: 50})
+    .then((posts) => {
+      console.log('Analyzing ' + posts.length + ' posts...');
+      posts.forEach(post => {
+        const links: Link[] = uniq(
+          getLinks(post.selftext).filter(link => !linkMatches(link, LINKS_TO_IGNORE))
+        );
+        if (links.length == 0) return;
+
+        const processedLinks = links.map(link => {
+          const gameLink = GAME_LINKS.find(gameLink => 
+            gameLink.linkMatchers.some(match => link.url.includes(match))
+          );
+          return {gameLink, link};
+        });
+
+        const [categorizedLinks, uncategorizedLinks] = partition(processedLinks, pl => pl.gameLink);
+
+        allUncategorizedLinks = allUncategorizedLinks.concat(
+          uncategorizedLinks.map(cl => cl.link)
+        );
+
+        if (categorizedLinks.length > 0) {
+          const potentialNames = uniq([
+            post.title,
+            ...categorizedLinks.map(cl => cl.link.text),
+          ]);
+
+          newGames.push({
+            name: potentialNames[0],
+            source: 'http://reddit.com' + post.permalink,
+            otherNames: potentialNames.slice(1),
+            links: categorizedLinks.map(cl => ({
+              link: cl.link.url,
+              platform: cl.gameLink?.platform!,
+            })),
+          });
+        }
+      });
+      
+      return true;
+    });
+
+  await Promise.all([parsePosts]);
+
+  console.log('Saving report');
+  if (newGames.length > 0) {
+    console.log('Found ' + newGames.length + ' new games. Look in new-games.json.');
+  }
+  fs.writeFileSync('new-games.json', JSON.stringify(newGames, null, 2));
+
+  if (allUncategorizedLinks.length > 0) {
+    console.log('Found ' + allUncategorizedLinks.length + ' uncategorized links. Look in uncategorizedLinks.json.');
+  }
+  fs.writeFileSync('uncategorizedLinks.json', JSON.stringify(allUncategorizedLinks, null, 2));
+})();
+
+function getLinks(body: string) {
+  return [...body.matchAll(new RegExp(LINK_REGEX, 'g'))]
+    .map(match => ({
+      text: match[1],
+      url: match[2],
+    }));
+}
+
+function linkMatches(link: Link, matches: string[]) {
+  return matches.some(match => link.url.includes(match));
+}
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,47 @@
-export interface Game {
+import { Features } from "snoowrap/dist/objects/RedditUser";
+
+export interface NewGame {
   name: string;
-  links: string[];
+  source: string;
+  otherNames: string[];
+  links: GameLink[];
+}
+
+export interface GameLink {
+  link: string;
+  platform: Platform,
+}
+
+export interface Link {
+  text: string;
+  url: string;
+}
+
+export interface Game {
+  id: number;
+  name: string,
+  style: GameStyle,
+  features: GameFeature[]
+  platforms: Record<Platform, {
+    id: string,
+    link: string,
+  }>
 }
 
 export enum Platform {
-  Steam,
-  IOS,
-  Android,
-  Web,
+  Steam = 'Steam',
+  IOS = 'IOS',
+  Android = 'Android',
+  Web = 'Web',
+  Windows = 'Windows',
+}
+
+export enum GameStyle {
+  Generic = "Generic",
+  GroundHogDay = "GroundHogDay",
+}
+
+export enum GameFeature {
+  Prestige = "Prestige",
+  Unfolding = "Unfolding",
 }
